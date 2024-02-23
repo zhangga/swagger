a967eb3d7a76cf7a59e3b1f872cac6ee9d83d45d
zeqiang.zhang <383523842@qq.com>
2024-02-23 18:20:02 +0800

    feat: 启动函数增加测试

--- hotfix/example/main.go
+++ hotfix/example/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"unsafe"
+
 	"github.com/zhangga/swagger/hotfix/example/originfunc"
 )
 
 func main() {
 	println(originfunc.GetSpecialName(1))
+
+	//ch := make(chan struct{})
+	//ch <- struct{}{}
+
+	buf := new([]byte)
+	copy(*buf, []byte("hello"))
+	size := unsafe.Sizeof(htest{})
+	fsize := uintptr(-int(size))
+	ffsize := fsize & 0x7
+	println(size + ffsize)
+}
+
+type htest struct {
+	age uint32
 }
